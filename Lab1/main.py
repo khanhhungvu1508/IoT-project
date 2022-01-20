5c55653b12126b6cfe6ed7a82b43be8ca5a5dd9a
khanhhungvu1508 <khanhhungvu1508@gmail.com>
2022-01-20 15:13:34 +0700

    Update source code lab 1 time 2 + Chromedriver file

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,6 +2,13 @@ print("Hello Thingboard")
 import paho.mqtt.client as mqttclient
 import time
 import json
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
@@ -40,16 +47,54 @@ client.loop_start()
 
 client.on_subscribe = subscribed
 client.on_message = recv_message
-        
+
+def getLocationByChromeDriver():
+    # All comment below is the code which older version, it easily deprecated
+    options = Options()
+    options.add_argument("--use-fake-ui-for-media-stream")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_experimental_option('useAutomationExtension', False)
+    timeout = 20
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s, options=options)
+    #driver = webdriver.Chrome(executable_path = './chromedriver.exe', chrome_options=options)
+    driver.get("https://mycurrentlocation.net/")
+    wait = WebDriverWait(driver, timeout)
+    time.sleep(3)
+    #longitude = driver.find_elements_by_xpath('//*[@id="longitude"]')
+    #longitude = [x.text for x in longitude]
+    #longitude = str(longitude[0])
+
+    longitude = driver.find_element(By.XPATH, '//*[@id="longitude"]').text
+
+    #latitude = driver.find_elements_by_xpath('//*[@id="latitude"]')
+    #latitude = [x.text for x in latitude]
+    #latitude = str(latitude[0])
+
+    latitude = driver.find_element(By.XPATH, '//*[@id="latitude"]').text
+
+    driver.quit()
+    return [latitude,longitude]
+
+def getLocationByThirdParty():
+    GPSInfo = requests.get('https://ipinfo.io/')
+    GPS = GPSInfo.json()
+    print(GPS)
+    return GPS['loc'].split(',')
 
 temp = 30
 humi = 50
-longitude = 106.6297
-latitude = 10.8231
 counter = 0
 while True:
+    # Find latitude and longitude by GPS location
+    #[latitude, longitude] = getLocationByThirdParty()
+    [latitude, longitude] = getLocationByChromeDriver()
+    # Debug line
+    print('longitude = ', longitude)
+    print('latitude = ', latitude)
     collect_data = {'temperature': temp, 'humidity': humi, 'longitude': longitude, 'latitude': latitude}
     temp += 1
     humi += 1
     client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
     time.sleep(10)
+
